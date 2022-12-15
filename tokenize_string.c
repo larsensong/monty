126e6c6e2e7d46894adca049e8e3d21b2403906d
larsensong <larsenmulamba@gmail.com>
2022-12-15 10:44:35 -0800

    push

--- tokenize_string.c
+++ tokenize_string.c
@@ -227,3 +227,105 @@ int get_word_length(char *str, char *delims)
 }
 
 
+
+/**
+ *
+ *  * get_word_count - gets the word count of a string
+ *
+ *   *
+ *
+ *    * @str: string to get word count from
+ *
+ *     * @delims: delimitors to use to delimit words
+ *
+ *      *
+ *
+ *       * Return: the word count of the string
+ *
+ *        */
+
+
+
+int get_word_count(char *str, char *delims)
+
+{
+
+		int wc = 0, pending = 1, i = 0;
+
+
+
+			while (*(str + i))
+
+					{
+
+								if (is_delim(str[i], delims))
+
+												pending = 1;
+
+										else if (pending)
+
+													{
+
+																	pending = 0;
+
+																				wc++;
+
+																						}
+
+												i++;
+
+													}
+
+				return (wc);
+
+}
+
+
+
+/**
+ *
+ *  * get_next_word - gets the next word in a string
+ *
+ *   *
+ *
+ *    * @str: string to get next word from
+ *
+ *     * @delims: delimitors to use to delimit words
+ *
+ *      *
+ *
+ *       * Return: pointer to first char of next word
+ *
+ *        */
+
+
+
+char *get_next_word(char *str, char *delims)
+
+{
+
+		int pending = 0;
+
+			int i = 0;
+
+
+
+				while (*(str + i))
+
+						{
+
+									if (is_delim(str[i], delims))
+
+													pending = 1;
+
+											else if (pending)
+
+															break;
+
+													i++;
+
+														}
+
+					return (str + i);
+
+}
